ef482d980c64a1f01b316d443a94aa7d9cc4a385
ansari.majid <majidgt786@gmail.com>
2023-06-20 15:39:52 +0530

    remove middleware add decorators

--- main_app/middleware.py
+++ main_app/middleware.py
@@ -30,15 +30,15 @@ from django.urls import reverse
 from django.shortcuts import redirect
 
 
-class FeePaymentMiddleware(MiddlewareMixin):
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        modulename = view_func.__module__
-        user = request.user
+# class FeePaymentMiddleware(MiddlewareMixin):
+#     def process_view(self, request, view_func, view_args, view_kwargs):
+#         modulename = view_func.__module__
+#         user = request.user
 
-        if user.is_authenticated and user.user_type == '3':  # Assuming 'user_type' identifies a student
-            if modulename == 'main_app.views' and not user.has_paid_fees() and user.payment_required:
-                user.payment_required = False  # Reset the payment_required flag
-                user.save()
-                return redirect(reverse('payment_required'))  # Redirect to a payment page or an error page
+#         if user.is_authenticated and user.user_type == '3':  # Assuming 'user_type' identifies a student
+#             if modulename == 'main_app.views' and not user.has_paid_fees() and user.payment_required:
+#                 user.payment_required = False  # Reset the payment_required flag
+#                 user.save()
+#                 return redirect(reverse('payment_required'))  # Redirect to a payment page or an error page
 
-        return None
+#         return None
--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,50 @@ from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from .EmailBackend import EmailBackend
+from functools import wraps
+from django.shortcuts import redirect, reverse
+
+from django.contrib.auth import logout
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.auth import logout
+
+from django.contrib.auth import logout
+
+from django.contrib.auth import logout
+
+from functools import wraps
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from .EmailBackend import EmailBackend
+
+def require_fee_payment(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        user = request.user
+
+        if user.is_authenticated and user.user_type == '3':  # Assuming 'user_type' identifies a student
+            if not user.has_paid_fees() and user.payment_required:
+                # Reset the payment_required flag
+                user.payment_required = False
+                user.save()
+
+                # Logout the user and terminate the session
+                logout_user(request)
+
+                return redirect(reverse_lazy('payment_required'))  # Redirect to a payment page or an error page
+
+        # Check and update payment status during each request
+        if user.is_authenticated and not user.has_paid_fees():
+            # Logout the user and terminate the session
+            logout_user(request)
+
+            return redirect(reverse_lazy('payment_required'))  # Redirect to a payment page or an error page
+
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
 
 
 def login_page(request):
@@ -22,7 +66,7 @@ def login_page(request):
                 return redirect(reverse("payment_required"))  # Redirect to a payment page or an error page
     return render(request, 'main_app/login.html')
 
-
+@require_fee_payment
 def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
